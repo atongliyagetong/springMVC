96f179b1d726295b24a151fc3be2981f5b401c4e
atongliyagetong <shanglushashen@gmai.com>
2017-08-09 21:11:43 +0800

    initial commit

---
+++ src/main/java/com/chen/entity/Goods.java
@@ -0,0 +1,41 @@
+package com.chen.entity;
+
+public class Goods {
+	private String id;
+	private String goodsNo;
+	private String goodsName;
+	private String price;
+	private String createTime;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getGoodsNo() {
+		return goodsNo;
+	}
+	public void setGoodsNo(String goodsNo) {
+		this.goodsNo = goodsNo;
+	}
+	public String getGoodsName() {
+		return goodsName;
+	}
+	public void setGoodsName(String goodsName) {
+		this.goodsName = goodsName;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	
+	
+}
